37d4c327d24d0aee027ee0602607de6a62698946
Brendan McCray <rbmccr@gmail.com>
2018-12-01 14:44:40 -0600

    game data (no shots yet) can be posted successfully to database

--- src/scripts/gameData.js
+++ src/scripts/gameData.js
@@ -1,5 +1,5 @@
 // import shotData from "./shotData"
-// import API from "./API"
+import API from "./API"
 
 // the purpose of this module is to:
 // 1. save all content in the gameplay page (shot and game data) to the database
@@ -13,22 +13,23 @@ const gameData = {
   //TODO: add function that defaults the game type (3v3 etc) to 2v2 and will toggle classes "is-selected" and "is-link" when any of the buttons is clicked.
 
   saveData() {
-    console.log("saving...")
+
+    // get user ID from session storage
+    // package and save game data
+    // TODO: then get ID of latest game saved (returned immediately in object from POST)
+    // TODO: package and save shots with the game ID
+    // TODO: conditional statement to prevent blank score entries
+
+    // playerId
+    const activeUserId = Number(sessionStorage.getItem("activeUserId"));
+
+    // game type (1v1, 2v2, 3v3)
     const btn_3v3 = document.getElementById("_3v3");
     const btn_2v2 = document.getElementById("_2v2");
     const btn_1v1 = document.getElementById("_1v1");
     const gameTypeBtns = [btn_3v3, btn_2v2, btn_1v1];
     let gameType = undefined;
 
-    // get user ID from session storage
-    // TODO: package and save game data
-    // TODO: then get ID of latest game saved (returned immediately in object from POST)
-    // TODO: package and save shots with the game ID
-
-    // playerId
-    const activeUserId = Number(sessionStorage.getItem("activeUserId"));
-
-    // game type (1v1, 2v2, 3v3)
     gameTypeBtns.forEach(btn => {
       console.log("btn", btn)
       if (btn.classList.contains("is-selected")) {
@@ -36,19 +37,56 @@ const gameData = {
       }
     })
 
+    // game mode (note: did not use boolean in case more game modes are supported in the future)
+    const sel_gameMode = document.getElementById("gameModeInput");
+    const gameMode = sel_gameMode.value.toLowerCase();
+
+    // my team (note: did not use boolean in preparation for users to enter the club information)
+    const sel_teamColor = document.getElementById("teamInput");
+    let myTeam;
+    if (sel_teamColor.value === "Orange team") {
+      myTeam = "orange";
+    } else {
+      myTeam = "blue";
+    }
+
+    // my score
+    let myScore;
+    let theirScore;
+    const inpt_orangeScore = document.getElementById("orangeScoreInput");
+    const inpt_blueScore = document.getElementById("blueScoreInput");
+
+    if (myTeam === "orange") {
+      myScore = Number(inpt_orangeScore.value);
+      theirScore = Number(inpt_blueScore.value);
+    } else {
+      myScore = Number(inpt_blueScore.value);
+      theirScore = Number(inpt_orangeScore.value);
+    }
+
+    // overtime
+    let overtime;
+    const sel_overtime = document.getElementById("overtimeInput");
+    if (sel_overtime.value === "Overtime") {
+      overtime = true;
+    } else {
+      overtime = false;
+    }
+
+
     let gameData = {
       "playerId": activeUserId,
-      // "mode": "Competitive"
+      "mode": gameMode,
       "type": gameType,
-      //   "blue": ,
-      //   "score": ,
-      //   "opp_score": ,
-      //   "overtime":
+      "team": myTeam,
+      "score": myScore,
+      "opp_score": theirScore,
+      "overtime": overtime
     };
 
     console.log(gameData)
 
-    // API.postItem(gameData, "games").then(data => console.log(data));
+    API.postItem(gameData, "games").then(data => console.log(data));
 
   },
 
@@ -58,23 +96,17 @@ const gameData = {
 
   editPrevGame() {
 
+    //TODO: allow user to edit content from most recent game saved (consider both MAX gameId and CURRENT userId to get the user's most recent game)
+
   }
 
   // const btn_editPrevGame = document.getElementById("editPrevGame");
-  // const btn_3v3 = document.getElementById("_3v3");
-  // const btn_2v2 = document.getElementById("_2v2");
-  // const btn_1v1 = document.getElementById("_1v1");
 
   // // select dropdowns
   // const sel_aerial = document.getElementById("aerialInput");
-  // const sel_gameMode = document.getElementById("gameModeInput");
-  // const sel_teamColor = document.getElementById("teamInput");
-  // const sel_overtime = document.getElementById("overtimeInput");
 
   // // input fields
   // const inpt_ballSpeed = document.getElementById("ballSpeedInput");
-  // const inpt_orangeScore = document.getElementById("orangeScoreInput");
-  // const inpt_blueScore = document.getElementById("blueScoreInput");
 
 
 }
