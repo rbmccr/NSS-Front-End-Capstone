92ce8a9d96d4ba5f79b6f63aa094146b2c18a28b
Brendan McCray <rbmccr@gmail.com>
2018-12-12 12:08:06 -0600

    added icon for total shots. downloaded several icons of different colors

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -14,31 +14,57 @@ const profile = {
   },
 
   buildProfile(user) {
-    // media card container profile picture, car photo, name, username, and member since mm/dd/yyyy
 
+    // media containers showing user stats
+
+    // card container profile picture, car photo, name, username, and member since mm/dd/yyyy
+
+    // const totalGames = elBuilder("div", { "class": "media" }, null, left2)
+
+    const icon1 = elBuilder("img", { "src":"images/icons/icons8-soccer-ball-96.png" }, null)
+    const iconParent1 = elBuilder("figure", { "class": "image is-64x64" }, null, icon1)
+    const left1 = elBuilder("div", { "class": "media-left" }, null, iconParent1);
+    const totalGoals = elBuilder("div", { "class": "media", "style":"padding:20px;" }, null, left1)
+
+    //   <article class="media">
+    // <figure class="media-left">
+    //   <p class="image is-64x64">
+    //     <img src="https://bulma.io/images/placeholders/128x128.png">
+    //   </p>
+    // </figure>
+    // <div class="media-content">
+    //   <div class="content">
+    //     <p>
+    //       <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+    //       <br>
+    //       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+    //     </p>
+    //   </div>
 
     // profile content
-    let carImgVariable = `images/cars/${user.car.toLowerCase()}.jpg`;
+    let carImgVariable = user.car.toLowerCase();
     let profileImgVariable = user.picture;
+    let profileImgTitle = user.picture;
     if (user.picture === "") {
       profileImgVariable = "images/profile-placeholder.jpg"
+      profileImgTitle = "profile-placeholder.jpg"
     }
     let memberSinceDateFormatted = new Date(user.joined).toLocaleString().split(",")[0];
 
-    const memberSince = elBuilder("div",{"class":"subtitle is-6", "style":"margin-top:10px"}, `Became a hotshot on ${memberSinceDateFormatted}`)
+    const memberSince = elBuilder("div", { "class": "subtitle is-6", "style": "margin-top:10px" }, `Became a hotshot on ${memberSinceDateFormatted}`)
     const username = elBuilder("div", { "class": "tag" }, `@${user.username}`);
     const name = elBuilder("div", { "class": "title is-4 is-marginless" }, `${user.name}`);
     const userInfo = elBuilder("div", { "class": "media-content" }, null, name, username, memberSince);
-    const carImg = elBuilder("img", { "src": `${carImgVariable}`, "alt": "car" }, null);
+    const carImg = elBuilder("img", { "src": `images/cars/${carImgVariable}.jpg`, "alt": "car", "title": `${carImgVariable}` }, null);
     const carImgFigure = elBuilder("figure", { "class": "image is-96x96" }, null, carImg);
     const carImgParent = elBuilder("div", { "class": "media-left" }, null, carImgFigure);
     const media = elBuilder("div", { "class": "media" }, null, carImgParent, userInfo);
     const content = elBuilder("div", { "class": "card-content" }, null, media);
     // main profile picture
-    const Img = elBuilder("img", { "src": `${profileImgVariable}`, "alt": "profile picture" });
+    const Img = elBuilder("img", { "src": `${profileImgVariable}`, "alt": "profile picture", "title": `${profileImgTitle}` });
     const figure = elBuilder("figure", { "class": "image" }, null, Img);
     const profilePicture = elBuilder("div", { "class": "card-image" }, null, figure);
-    const card = elBuilder("div", { "class": "card" }, null, profilePicture, content);
+    const card = elBuilder("div", { "class": "card" }, null, profilePicture, content, totalGoals);
 
     // parent containers that organize profile information into columns
     const blankColumnLeft = elBuilder("div", { "class": "column is-one-fourth" }, null);
@@ -47,13 +73,6 @@ const profile = {
     const columns = elBuilder("div", { "class": "columns" }, null, blankColumnLeft, profileColumn, blankColumnRight);
     const playerProfile = elBuilder("div", { "id": "profileContainer", "class": "container box" }, null, columns);
 
-
-
-
-    // const car = elBuilder("img", {"src": "images/octane.jpg", "class": "" })
-    // const name = elBuilder("div", {"class": "notification" }, `Name: ${user.name}`)
-    // const username = elBuilder("div", {"class": "notification" }, `Username: ${user.username}`)
-    // const playerProfile = elBuilder("div", {"id": "playerProfile", "class": "container" }, null, car, name, username)
     webpage.appendChild(playerProfile)
   }
 
