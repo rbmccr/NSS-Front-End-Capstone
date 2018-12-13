5b640092de8cdb58f6f09fad6a62cdc9db3f1b17
Brendan McCray <rbmccr@gmail.com>
2018-12-12 18:16:39 -0600

    finished styling login and signup forms. need to finish signup form email, timestamp, and car dropdown functionality

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -7,50 +7,13 @@ const webpageNav = document.getElementById("nav-master");
 
 const loginOrSignup = {
 
-  // build a login form that validates user input. Successful login stores user id in session storage
   loginForm() {
-
-    //   <div class="field is-horizontal">
-    //      <div class="field-label is-normal">
-    //        <label class="label">From</label>
-    //      </div>
-
-    //      <div class="field-body">
-    //        <div class="field">
-    //          <p class="control is-expanded has-icons-left">
-    //            <input class="input" type="text" placeholder="Name">
-    //            <span class="icon is-small is-left">
-    //              <i class="fas fa-user"></i>
-    //            </span>
-    //          </p>
-    //        </div>
-
-    //   <div class="field">
-    //     <p class="control is-expanded has-icons-left has-icons-right">
-    //       <input class="input is-success" type="email" placeholder="Email" value="alex@smith.com">
-    //       <span class="icon is-small is-left">
-    //         <i class="fas fa-envelope"></i>
-    //       </span>
-    //       <span class="icon is-small is-right">
-    //         <i class="fas fa-check"></i>
-    //       </span>
-    //     </p>
-    //   </div>
-
-
-    //     <div class="field">
-    //   <label class="label">Name</label>
-    //   <div class="control">
-    //     <input class="input" type="text" placeholder="Text input">
-    //   </div>
-    // </div>
-
-
+    // this function builds a login form that validates user input. Successful login stores user id in session storage
     const loginButton = elBuilder("button", { "id": "loginNow", "class": "button is-dark" }, "Login now");
     const loginBtnControl = elBuilder("div", { "class": "buttons is-centered" }, null, loginButton)
 
     // password input with icon
-    const loginPasswordIcon = elBuilder("i", { "class": "fas fa-key" })
+    const loginPasswordIcon = elBuilder("i", { "class": "fas fa-lock" })
     const loginPasswordIconDiv = elBuilder("span", { "class": "icon is-small is-left" }, null, loginPasswordIcon)
     const loginInput_password = elBuilder("input", { "id": "passwordInput", "class": "input", "type": "password", "placeholder": "enter password" });
 
@@ -68,7 +31,7 @@ const loginOrSignup = {
     const loginUsernameField = elBuilder("div", { "class": "field" }, null, loginUsernameLabel, loginUsernameControl)
 
     // form
-    const loginForm = elBuilder("form", { "id": "loginForm", "class": "box", "style":"margin-top:-57px" }, null, loginUsernameField, loginPasswordField, loginBtnControl);
+    const loginForm = elBuilder("form", { "id": "loginForm", "class": "box", "style": "margin-top:-57px; min-width:20%"}, null, loginUsernameField, loginPasswordField, loginBtnControl);
 
     webpage.innerHTML = null;
     // set style of master container to display flex to align forms in center of container
@@ -80,12 +43,65 @@ const loginOrSignup = {
   },
 
   signupForm() {
-    const signupInput_name = elBuilder("input", { "id": "nameInput", "class": "input", "type": "text", "placeholder": "enter name" });
-    const signupInput_username = elBuilder("input", { "id": "usernameInput", "class": "input", "type": "text", "placeholder": "enter username" });
-    const signupInput_password = elBuilder("input", { "id": "passwordInput", "class": "input", "type": "text", "placeholder": "enter password" });
-    const signupInput_confirm = elBuilder("input", { "id": "confirmPassword", "class": "input", "type": "text", "placeholder": "confirm password" });
     const signupButton = elBuilder("button", { "id": "signupNow", "class": "button is-dark" }, "Sign up now");
-    const signupForm = elBuilder("form", { "id": "signupForm", "class": "field box" }, null, signupInput_name, signupInput_username, signupInput_password, signupInput_confirm, signupButton);
+    const signupBtnControl = elBuilder("div", { "class": "buttons is-centered" }, null, signupButton)
+
+    // name input with icon
+    const signupNameIcon = elBuilder("i", { "class": "fas fa-pencil-alt" })
+    const signupNameIconDiv = elBuilder("span", { "class": "icon is-small is-left" }, null, signupNameIcon)
+    const signupInput_name = elBuilder("input", { "id": "nameInput", "class": "input", "type": "text", "placeholder": "enter name" });
+
+    const signupNameControl = elBuilder("div", { "class": "control has-icons-left" }, null, signupInput_name, signupNameIconDiv)
+    const signupNameLabel = elBuilder("label", { "class": "label" }, "Name")
+    const signupNameField = elBuilder("div", { "class": "field" }, null, signupNameLabel, signupNameControl)
+
+    // username input with icon
+    const signupUsernameIcon = elBuilder("i", { "class": "fas fa-user" })
+    const signupUsernameIconDiv = elBuilder("span", { "class": "icon is-small is-left" }, null, signupUsernameIcon)
+    const signupInput_username = elBuilder("input", { "id": "usernameInput", "class": "input", "type": "text", "placeholder": "enter username" });
+
+    const signupUsernameControl = elBuilder("div", { "class": "control has-icons-left" }, null, signupInput_username, signupUsernameIconDiv)
+    const signupUsernameLabel = elBuilder("label", { "class": "label" }, "Username")
+    const signupUsernameField = elBuilder("div", { "class": "field" }, null, signupUsernameLabel, signupUsernameControl)
+
+    // email input with icon
+    const signupEmailIcon = elBuilder("i", { "class": "fas fa-at" })
+    const signupEmailIconDiv = elBuilder("span", { "class": "icon is-small is-left" }, null, signupEmailIcon)
+    const signupInput_email = elBuilder("input", { "id": "emailInput", "class": "input", "type": "email", "placeholder": "enter email" });
+
+    const signupEmailControl = elBuilder("div", { "class": "control has-icons-left" }, null, signupInput_email, signupEmailIconDiv)
+    const signupEmailLabel = elBuilder("label", { "class": "label" }, "Email")
+    const signupEmailField = elBuilder("div", { "class": "field" }, null, signupEmailLabel, signupEmailControl)
+
+    // password input with icon
+    const signupPasswordIcon = elBuilder("i", { "class": "fas fa-lock" })
+    const signupPasswordIconDiv = elBuilder("span", { "class": "icon is-small is-left" }, null, signupPasswordIcon)
+    const signupInput_password = elBuilder("input", { "id": "passwordInput", "class": "input", "type": "text", "placeholder": "enter password" });
+
+    const signupPasswordControl = elBuilder("div", { "class": "control has-icons-left" }, null, signupInput_password, signupPasswordIconDiv)
+    const signupPasswordLabel = elBuilder("label", { "class": "label" }, "Password")
+    const signupPasswordField = elBuilder("div", { "class": "field" }, null, signupPasswordLabel, signupPasswordControl)
+
+    // confirm password input with icon
+    const signupConfirmIcon = elBuilder("i", { "class": "fas fa-lock" })
+    const signupConfirmIconDiv = elBuilder("span", { "class": "icon is-small is-left" }, null, signupConfirmIcon)
+    const signupInput_confirm = elBuilder("input", { "id": "confirmPassword", "class": "input", "type": "email", "placeholder": "confirm password" });
+
+    const signupConfirmControl = elBuilder("div", { "class": "control has-icons-left" }, null, signupInput_confirm, signupConfirmIconDiv)
+    const signupConfirmLabel = elBuilder("label", { "class": "label" }, "Confirm Password")
+    const signupConfirmField = elBuilder("div", { "class": "field" }, null, signupConfirmLabel, signupConfirmControl)
+
+    // confirm password input with icon
+    const signupProfilePicIcon = elBuilder("i", { "class": "fas fa-image" })
+    const signupProfilePicIconDiv = elBuilder("span", { "class": "icon is-small is-left" }, null, signupProfilePicIcon)
+    const signupInput_profilePic = elBuilder("input", { "id": "profilePicURL", "class": "input", "type": "email", "placeholder": "provide a URL (optional)" });
+
+    const signupProfilePicControl = elBuilder("div", { "class": "control has-icons-left" }, null, signupInput_profilePic, signupProfilePicIconDiv)
+    const signupProfilePicLabel = elBuilder("label", { "class": "label" }, "Profile Picture")
+    const signupProfilePicField = elBuilder("div", { "class": "field" }, null, signupProfilePicLabel, signupProfilePicControl)
+
+    // form
+    const signupForm = elBuilder("form", { "id": "signupForm", "class": "box", "style":"min-width:20%" }, null, signupNameField, signupUsernameField, signupEmailField, signupPasswordField, signupConfirmField, signupProfilePicField, signupBtnControl);
 
     webpage.innerHTML = null;
     webpage.style.display = "flex";
